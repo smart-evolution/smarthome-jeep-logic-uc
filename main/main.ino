89afcbffaef754c17f84301f13d6def42c32e0ae
Oskar Szura <oskar.szura@gmail.com>
2019-04-01 20:05:08 +0200

    HOME-118 - Implement jeep uC logic

---
+++ main/main.ino
@@ -0,0 +1,80 @@
+#include <SoftwareSerial.h>
+
+SoftwareSerial softSerial(5, 6);
+const int moto1Enable = 9;
+const int moto1in1 = 19;
+const int moto1in2 = 17;
+const int moto2Enable = 10;
+const int moto2in1 = 16;
+const int moto2in2 = 18;
+
+String readString;
+
+void setup() {
+  Serial.begin(9600);
+  softSerial.begin(9600);
+
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
+}
+
+
+void loop() {
+  int incomingBytes = softSerial.available();
+
+  if (incomingBytes >= 6) {
+    handleIncomingMsg(incomingBytes);
+  }
+}
+
+void handleIncomingMsg(int incomingBytes) {
+  char cmd[6];
+  
+  for (int i = 0; i < incomingBytes; i++) {
+    cmd[i] = softSerial.read(); 
+  }
+
+  // 2
+  if (strcmp(cmd, "CMD020") == 0) {
+    setMotor(0, false, 2);
+  }
+  else if (strcmp(cmd, "CMD022") == 0) {
+    setMotor(255, false, 2);
+  }    
+  else if (strcmp(cmd, "CMD122") == 0) {
+    setMotor(255, true, 2);
+  }    
+  // 1
+  else if (strcmp(cmd, "CMD010") == 0) {
+    setMotor(0, false, 1);
+  }  
+  else if (strcmp(cmd, "CMD012") == 0) {
+    setMotor(255, false, 1);
+  }
+  else if (strcmp(cmd, "CMD112") == 0) {
+    setMotor(255, true, 1);
+  }
+  // others
+  else if (strcmp(cmd, "CMDWHO") == 0) {
+    softSerial.write("jeep");
+  }
+}
+
+void stopMotors() {
+  setMotor(0, false, 1);
+  setMotor(0, false, 2);
+}
+
+void setMotor(int speed, boolean reverse, int moto)
+{
+  if (moto == 1) {
+    analogWrite(moto1Enable, speed);
+    digitalWrite(moto1in1, !reverse);
+    digitalWrite(moto1in2, reverse);    
+  } else if (moto == 2) {
+    analogWrite(moto2Enable, speed);
+    digitalWrite(moto2in1, !reverse);
+    digitalWrite(moto2in2, reverse); 
+  }
+}
+
